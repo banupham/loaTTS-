1160b88dbcf2422a6edce2d1e63bd0158138b2c2
banupham <135731801+banupham@users.noreply.github.com>
2026-08-08 23:18:42 +0700

    Remove generic speak test

--- test_speak.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-curl -X POST http://127.0.0.1:8780/speak ^
-  -H "Content-Type: application/json" ^
-  -d "{\"text\":\"Xin chào. Loa TTS thường trực đang hoạt động.\",\"priority\":50,\"style\":\"tu_nhien\"}"
-echo.
-pause
